1af2aa173eb03fe9071e9859c79dd5f68c95b247
Demirath <nd194@msstate.edu>
2017-04-03 23:28:02 -0500

    git commit Basic GUI for testing

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour {
+
+    public float health = 500f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector3(health / 500f, 1);
+        transform.localPosition = new Vector3((health / 2f) - 250f, 0, 0);
+	}
+}
---
+++ Assets/Scripts/MenuBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehavior : MonoBehaviour {
+
+    public Canvas MainMenu;
+    public Canvas QuitMenu;
+
+	// Use this for initialization
+	void Start () {
+        QuitMenu.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape") && !MainMenu.enabled)
+            QuitMenu.enabled = true;
+    }
+
+    public void ShutDown ()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame ()
+    {
+        //game go here
+    }
+}
