88739aae06143b1359cd61bc86a8da17611812a2
Demirath <nd194@msstate.edu>
2017-04-11 00:39:32 -0500

    Sounds, backgrounds, menu flow, anims

--- Assets/Scripts/MenuBehavior.cs
+++ Assets/Scripts/MenuBehavior.cs
@@ -5,17 +5,49 @@ using UnityEngine;
 public class MenuBehavior : MonoBehaviour {
 
     public Canvas MainMenu;
+    public Canvas Lobbies;
+    public Canvas TeamPicker;
     public Canvas QuitMenu;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject orange;
+    public GameObject blue;
+
+    private bool team = true;
+
+
+    // Use this for initialization
+    void Start () {
+        Lobbies.enabled = false;
+        TeamPicker.enabled = false;
         QuitMenu.enabled = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("escape") && !MainMenu.enabled)
+        if (Input.GetKeyDown("escape") && !MainMenu.enabled && !Lobbies.enabled && !TeamPicker.enabled)
+        {
             QuitMenu.enabled = true;
+        }
+        if (team)
+        {
+            orange.GetComponent<RectTransform>().sizeDelta = new Vector2(184, 34.5f);
+            blue.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 30);
+        }
+        else
+        {
+            blue.GetComponent<RectTransform>().sizeDelta = new Vector2(184, 34.5f);
+            orange.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 30);
+        }
+    }
+
+    public void selOrange()
+    {
+        team = true;
+    }
+
+    public void selBlue()
+    {
+        team = false;
     }
 
     public void ShutDown ()
@@ -26,5 +58,10 @@ public class MenuBehavior : MonoBehaviour {
     public void StartGame ()
     {
         //game go here
+    }
+
+    public void StopGame()
+    {
+        //stop teh gamez
     }
 }
