ea7a0d5d30fd93e9fb82337bdf87d0140d651a29
Eli Davis <eli.davis1995@gmail.com>
2017-04-10 22:39:59 -0500

    imported demo version of mech

---
+++ Assets/Scripts/Scenes/EliTesting/MechAnimate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ISO.Scenes.EliTesting {
+
+	public class MechAnimate : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			transform.Translate (Vector3.up*Mathf.Sin(Time.time*2)*Mathf.PerlinNoise(0, Time.time)*.5f*Time.deltaTime);
+		}
+	}
+
+}
