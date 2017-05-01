60fd6616b5416dbbae151ec8b322e4d438abde09
JoshuaQChurch <JoshuaQChurch@gmail.com>
2017-05-01 10:45:49 -0500

    Updated menu and some sounds

--- Assets/Scripts/Managers/MainMenuController.cs
+++ Assets/Scripts/Managers/MainMenuController.cs
@@ -9,11 +9,6 @@ public class MainMenuController : MonoBehaviour {
     [SerializeField]
     private bool isStartMenu = true;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space) && isStartMenu)
